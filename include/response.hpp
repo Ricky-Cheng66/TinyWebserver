b58204747cfffe63489527773f6b1cb3b92486fe
Ricky <1976173356@qq.com>
2025-10-21 16:09:38 +0800

    feat: std::ifstream 替换文件读取，RAII 零泄漏

--- include/response.hpp
+++ include/response.hpp
@@ -1,2 +1,4 @@
 #include "server.hpp"
+#include <fstream>
+#include <sstream>
 std::string buildResponse(const std::string& path);
--- src/response.cpp
+++ src/response.cpp
@@ -3,23 +3,17 @@ std::string buildResponse(const std::string& path)
 {
     /* 读文件 */
     std::string file = "www" + path;
-    int filefd = open(file.c_str(), O_RDONLY);
+    std::ifstream in(file, std::ios::binary);
     std::string body;
-    if (filefd < 0)
+    if (!in)
     {
         body = "<h1>404 Not Found</h1>";
     }
     else
     {
-        struct stat st;
-        fstat(filefd, &st);
-        body.resize(st.st_size);
-        ssize_t n = read(filefd, &body[0], st.st_size);
-        if (n < 0)
-        {
-            perror("read failed...");
-        }
-        close(filefd);
+        std::ostringstream tmp;
+        tmp << in.rdbuf();
+        std::string body = tmp.str();
     }
     /* 构造响应 */
     std::string resp =
